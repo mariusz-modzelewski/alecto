a2abb88e1f39afec3721822d109e3d6d1cc14e3d
mariusz-modzelewski <m.modzelewski97@gmail.com>
2021-01-24 17:27:42 +0100

    styled pricing, locations and templates

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu__items');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+    menu.classList.toggle('menu__items--open');
+});
+
+const hideHamburger = (isDesktop) => {
+    if (isDesktop) {
+        menu.classList.remove('menu__items--open');
+        hamburger.classList.remove('hamburger--active');
+    }
+}
+
+let desktopViewport = window.matchMedia("screen and (min-width: 1024px)");
+
+desktopViewport.addListener(isDesktop => {
+    hideHamburger(isDesktop.matches);
+});
+
+
+const menuFixed = document.querySelector(".menu");
+
+window.addEventListener("scroll", () => {
+    if (document.scrollingElement.scrollTop > 150) {
+        menuFixed.classList.add("menu--fixed");
+    } else {
+        menuFixed.classList.remove("menu--fixed");
+    }
+});
