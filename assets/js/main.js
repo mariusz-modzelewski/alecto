3e6f0fcc577d2aca358e3d599dbbda28b096afb8
Mariusz Modzelewski <m.modzelewski97@gmail.com>
2021-06-08 21:35:56 +0200

    add slide change and change images to webp

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,43 @@ window.addEventListener("scroll", () => {
         menuFixed.classList.remove("menu--fixed");
     }
 });
+
+class Slider {
+    constructor() {
+        this.slides = null;
+        this.slidesDots = null;
+        this.slideButtons = null;
+        this.currentSlide = 1;
+        this.slidesLength = 0;
+    }
+
+    initializeSlider() {
+        this.slides = document.querySelectorAll('.slider__slide');
+        this.slidesDots = document.querySelectorAll('.slider__slide-dot');
+
+        this.slidesLength = document.querySelectorAll('.slider__slide').length;
+        this.slideButtons = document.querySelectorAll('slider__slide-dot');
+
+        setInterval(() => {
+            this.currentSlide++;
+
+            const slide = document.querySelector('.slider__slide.slider__slide--active');
+            const slideDot = document.querySelector('.slider__slide-dot.slider__slide-dot--active');
+            slide.classList.remove('slider__slide--active');
+            slideDot.classList.remove('slider__slide-dot--active');
+
+
+            if(this.currentSlide > this.slidesLength) {
+                this.slides[0].classList.add('slider__slide--active');
+                this.slidesDots[0].classList.add('slider__slide-dot--active');
+                this.currentSlide = 1;
+            } else {
+                slide.nextElementSibling.classList.add('slider__slide--active');
+                slideDot.nextElementSibling.classList.add('slider__slide-dot--active');
+            }
+        }, 2000);
+    }
+}
+
+const slider = new Slider();
+slider.initializeSlider();
